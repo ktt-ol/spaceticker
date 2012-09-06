34165e67dd0caf36fc245a89ea27169fe51f7359
Oliver Tonnhofer <olt@bogosoft.com>
2012-09-06 21:53:13 +0200

    update text

--- ticker.ino
+++ ticker.ino
@@ -28,21 +28,27 @@ void setup() {
     font::setFont(font_helvB10);
     prev = &buffer1;
     next = &buffer2;
-    prev->randomize(3);
+    prev->randomize(5);
 }
 
-char *msg = { 
-    "Sommer im Quartier."
-    "\tKreativitaet trifft Technik!"
-    "\tDer Oldenburger Hackspace stellt sich vor."
-    "\tBasteln, Loeten und Lernen!   3D-Drucker, Styroporschneider, LED-Cubes,"
-    " Klackerlaken, Arduino, CNC-Fraese, Terminals"
-    "\tDer Space ist offen! Kommt einfach rein und Treppe hoch."
+char *msg = {
+    "   Kreativitaet trifft Technik!"
+    "\tDer Oldenburger Hackspace."
+    "\t3D-Drucker, Styroporschneider, LED-Cubes, Arduino, "
+    " CNC-Fraese, Saegen, Schrauben, Loeten und Programmieren."
+    "\tOffen wenn offen. Siehe: http://status.ktt-ol.de"
+    "\tMindestens: Di, Do, Sa, So ab 18:00 Uhr"
+    "\thttp://ktt-ol.de"
 };
+
+void blank() {
+    ticker.shiftString(" ", 5);
+}
 
 
 int counter = 0;
-void loop() {
+
+void gameOfLiveLoop() {
     gameOfLive(prev, next);
     if ((counter % 20) == 0) {
         randomGlider(next);
@@ -54,10 +60,14 @@ void loop() {
     tmp = next;
     next = prev;
     prev = tmp;
+}
 
-    counter += 1;
-
-    // ticker.shiftString(msg, 20);
-    // updateDisplayFromSerial(&buffer1);
-    // ticker.shiftInDisplayBufferRaw(&buffer1);
+void loop() {
+    blank();
+    prev->randomize(5);
+    for (int counter = 0; counter < 30; ++counter) {
+        gameOfLiveLoop();
+    }
+    blank();
+    ticker.shiftString(msg, 12);
 }
