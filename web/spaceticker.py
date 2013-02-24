cfd9533c8661c1dfa93e78cd9208a4f911ee56bd
Oliver Tonnhofer <olt@bogosoft.com>
2013-02-24 17:05:46 +0100

    add web interface

---
+++ web/spaceticker.py
@@ -0,0 +1,61 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask('spaceticker')
+app.debug = True
+
+import logging
+log = logging.getLogger('spaceticker')
+
+class Spaceticker(object):
+    def __init__(self):
+        self.serial = None
+
+    def status(self, is_open):
+        if is_open:
+            cmd = 'so\n'
+        else:
+            cmd = 'sc\n'
+        self.serial.write(cmd)
+
+    def send(self, msg):
+        cmd = 'm%s\n' % msg
+        self.serial.write(cmd)
+
+@app.route('/')
+def index():
+    return render_template('index.html', is_open=current_status)
+
+@app.route('/msg')
+def msg():
+    msg = request.args.get('s')
+    ticker.send(msg)
+    return redirect(url_for('index'))
+
+current_status = False
+ticker = Spaceticker()
+
+def init_app(serial_dev=None):
+    import serial
+    if serial_dev:
+        ser = serial.Serial(serial_dev, 115200)
+    else:
+        class Dummy():
+            def write(self, text):
+                print text
+        ser = Dummy()
+    ticker.serial = ser
+    def status_callback(is_open):
+        global current_status
+        current_status = is_open
+        ticker.status(current_status)
+    from status import SpaceStatus
+    s = SpaceStatus(status_callback)
+    s.start()
+    return app
+
+
+
+if __name__ == '__main__':
+    import sys
+    app = init_app(sys.argv[1])
+    app.run(host="0.0.0.0")
---
+++ web/status.py
@@ -0,0 +1,66 @@
+import requests
+import json
+import socket
+import threading
+import time
+
+STATUS_URL = "http://status.kreativitaet-trifft-technik.de/status-stream"
+
+import logging
+log = logging.getLogger('spaceticker')
+
+def parsed_events(events):
+    for event_lines in events:
+        resp = {}
+        for line in event_lines:
+            key, value = line.split(': ', 1)
+            try:
+                resp[key] = json.loads(value)
+            except ValueError:
+                resp[key] = value
+        yield resp
+
+def event_chunks(resp):
+    last_event_data = []
+    for line in resp.iter_lines(1):
+        if not line:
+            if last_event_data:
+                yield last_event_data
+            last_event_data = []
+        else:
+            last_event_data.append(line)
+
+class SpaceStatus(threading.Thread):
+    def __init__(self, status_callback):
+        threading.Thread.__init__(self)
+        self.daemon = True
+        self.status_callback = status_callback
+
+    def run(self):
+        while True:
+            try:
+                resp = requests.get(STATUS_URL, stream=True, timeout=30)
+                for event in parsed_events(event_chunks(resp)):
+                    if event['event'] == 'status':
+                        self.status_callback(event['data']['open'])
+            except socket.timeout:
+                continue
+            except requests.RequestException, ex:
+                log.error('request error: %s' % ex)
+                time.sleep(10)
+            except Exception:
+                log.exception('unknown request error:')
+                time.sleep(60)
+
+if __name__ == '__main__':
+    def status_callback(open):
+        if open:
+            print "It's open!"
+        else:
+            print "It's closed!"
+    s = SpaceStatus(status_callback)
+    s.start()
+    try:
+        time.sleep(60)
+    except (KeyboardInterrupt, SystemExit):
+        pass
