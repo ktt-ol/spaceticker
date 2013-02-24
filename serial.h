ebc05b31e3f76c24ec590eebef5422c4276d9a3e
Oliver Tonnhofer <olt@bogosoft.com>
2013-02-24 15:42:28 +0100

    working version with read_command

--- serial.h
+++ serial.h
@@ -7,8 +7,9 @@ static const byte MAGIC_BYTES[4] = {159, 170, 85, 241};
 
 #define SERIAL_WAIT_AVAILABLE() while (Serial.available() < 1) {}
 
-byte seekToFrameStart() {
-    while (1) {
+bool seekToFrameStart() {
+    uint32_t timeout = millis() + 1000;
+    while (millis() < timeout) {
         Serial.print('\n');
         SERIAL_WAIT_AVAILABLE();
         // read till first magic byte
@@ -23,19 +24,22 @@ byte seekToFrameStart() {
         SERIAL_WAIT_AVAILABLE();
         if (Serial.read() != MAGIC_BYTES[3]) { continue; }
         SERIAL_WAIT_AVAILABLE();
-        return Serial.read();
+        return true;
     }
+    return false;
 }
 
-byte updateDisplayFromSerial(DisplayBuffer_t *display) {
-    seekToFrameStart();
+bool updateDisplayFromSerial(DisplayBuffer_t *display) {
+    if (!seekToFrameStart()) {
+        return false;
+    }
     int bytesRead = 0;
     while (bytesRead <= display->size) {
         SERIAL_WAIT_AVAILABLE();
         display->buf[bytesRead] = Serial.read();
         bytesRead += 1;
     }
-    return 0;
+    return true;
 }
 
 #endif
--- spaceticker.ino
+++ spaceticker.ino
@@ -17,16 +17,20 @@ RandomFade_t fade;
 GOLTask_t gol_task;
 
 const uint16_t MAX_MSG_SIZE = 512;
-const char MAGIC_EEPROM_BYTE = 211;
-// char msg[MAX_MSG_SIZE] = {
-//     "   Mainframe  -  "
-//     "Der Oldenburger Hackspace."
-//     "\tTaeglich ab 18:00 Uhr. Siehe: http://status.ktt-ol.de\t\t\t\t"
-// };
 
-char msg[MAX_MSG_SIZE] = {
-    "123456789012345678901234567890"
+int space_status;
+
+char header[] = {
+    "   Mainframe  -  "
+    "Der Oldenburger Hackspace.\t"
 };
+char open[] = {
+    "Der Space ist offen!\t"
+};
+char closed[] = {
+    "Taeglich ab 18:00 Uhr. Siehe: http://status.ktt-ol.de\t"
+};
+char msg[MAX_MSG_SIZE];
 
 void read_msg_from_eeprom() {
     uint16_t pos = 0;
@@ -53,13 +57,6 @@ void write_msg_to_eeprom() {
     EEPROM.write(pos, '\0');
 }
 
-// void blink() {
-//     for (int i = 0; i < 10; ++i) {
-//         digitalWrite(13, !digitalRead(13));
-//         delay(50);
-//     }
-// }
-
 void terminate_message(uint16_t num_bytes) {
     msg[num_bytes >= MAX_MSG_SIZE ? MAX_MSG_SIZE - 1 : num_bytes] = '\0';
     for (int i = 0; i < MAX_MSG_SIZE; ++i) {
@@ -73,43 +70,41 @@ void terminate_message(uint16_t num_bytes) {
     }
 }
 
-bool cli_loop() {
+bool in_image_mode = false;
+
+/** Read commands from serial.
+*
+* @return true if command changed (i.e. from image mode to text)
+*/
+bool read_commands() {
     if (Serial.available() > 0) {
         int type = Serial.read();
         if (type == 'm') {
-            Serial.println("reading msg");
             uint16_t bytes_read = Serial.readBytesUntil('\n', msg, MAX_MSG_SIZE);
             terminate_message(bytes_read);
-            Serial.print("msg: ");
-            Serial.println(msg);
-            delay(50);
             write_msg_to_eeprom();
+        } else if (type == 's') {
+            while (Serial.available() <= 0) { }
+            space_status = Serial.read();
+        } else if (type == 'i') {
+            if (in_image_mode) {
+                return false;
+            }
+
+            in_image_mode = true;
             return true;
         }
+        if (in_image_mode) {
+            in_image_mode = false;
+            return true;
+        }
+        return false;
     }
     return false;
-    // delay(500);
-    // Serial.println(msg);
-    // delay(500);
 }
-
-int counter = 0;
-
-void gameOfLiveLoop() {
-    gol_step(prev, next);
-    if ((counter % 20) == 0) {
-        gol_random_glider(next);
-    }
-    if ((counter % 20) == 10) {
-        gol_random_fpentomino(next);
-    }
-    ticker_shift_display_buffer(next);
-    disp_swap(&prev, &next);
-}
-
 
 uint32_t next_time = 0;
-const uint8_t TICK_INTERVAL = 15;
+const uint8_t TICK_INTERVAL = 12;
 
 void frame_delay_init(void) {
     next_time = millis() + TICK_INTERVAL;
@@ -123,8 +118,48 @@ void frame_delay(void) {
     next_time = millis() + TICK_INTERVAL;
 }
 
+
+enum LoopReturn {
+    NORMAL,
+    COMMAND_CHANGE
+};
+#define READ_COMMAND() if(read_commands()) { return COMMAND_CHANGE; }
+#define PASS_COMMAND_CHANGE(x) if (x == COMMAND_CHANGE) { return COMMAND_CHANGE; }
+
+LoopReturn gol_loop() {
+    // fade in random
+    disp_randomize_factor(prev, 5);
+    fade_init(&fade, next, prev);
+    frame_delay_init();
+    while (fade_step(&fade)) {
+        ticker_shift_display_buffer(next);
+        READ_COMMAND();
+        frame_delay();
+    }
+
+    // game of life with current random display
+    gol_task_init(&gol_task, prev, next, 50);
+    while(gol_task_step(&gol_task)) {
+        READ_COMMAND();
+        ticker_shift_display_buffer(gol_task.next);
+    }
+
+    // fade out game of life
+    disp_erase_value(prev, 0);
+    fade_init(&fade, next, prev);
+    frame_delay_init();
+    while (fade_step(&fade)) {
+        ticker_shift_display_buffer(next);
+        READ_COMMAND();
+        frame_delay();
+    }
+    return NORMAL;
+}
+
+
 void setup() {
-    Serial.begin(250000);
+    // Serial.begin(250000);
+    Serial.begin(115200);
     Serial.write('?');
     ticker_init_pins();
     pinMode(13, OUTPUT);
@@ -136,37 +171,60 @@ void setup() {
     read_msg_from_eeprom();
 }
 
+LoopReturn image_loop() {
+    if (updateDisplayFromSerial(next)) {
+        ticker_shift_display_buffer(next);
+    }
+    while (true) {
+        READ_COMMAND();
+    }
+}
 
-void loop1() {
+LoopReturn shift_msg(char *msg) {
     font_string_shift_init(&shift_info, next, &font_info, msg);
     frame_delay_init();
     while (font_string_shift_step(&shift_info)) {
+        READ_COMMAND();
         ticker_shift_display_buffer(next);
         frame_delay();
-        if (cli_loop()) {
-            break;
-        }
     }
-    // font_string_shift_step returns immediately when msg
-    // is empty, call cli_loop again
-    cli_loop();
+    ticker_shift_display_buffer(next);
+    font_string_shift_reset(&shift_info);
+    return NORMAL;
 }
 
-void image_loop() {
-    updateDisplayFromSerial(next);
-    ticker_shift_display_buffer(next);
+
+/** Normal text/anim loop.
+*
+* Displays header text, open/close status, custom msg,
+* Game Of Live loop and URL.
+*/
+LoopReturn text_loop() {
+    PASS_COMMAND_CHANGE(shift_msg(header));
+    if (space_status == 'o') {
+        PASS_COMMAND_CHANGE(shift_msg(open));
+    } else {
+        PASS_COMMAND_CHANGE(shift_msg(closed));
+    }
+    PASS_COMMAND_CHANGE(shift_msg(msg));
+    PASS_COMMAND_CHANGE(shift_msg("\t http://mainframe.io"));
+    PASS_COMMAND_CHANGE(shift_msg(" \t\t\t\t"));
 }
+
 
 void loop() {
-    updateDisplayFromSerial(next);
-    ticker_shift_display_buffer(next);
+    while (true) {
+        if (in_image_mode) {
+            image_loop();
+        } else {
+            text_loop();
+            gol_loop();
+        }
+    }
 }
 
-void foo() {
-    disp_erase_value(next, 0);
-    disp_erase_value(prev, 0);
-    ticker_shift_display_buffer(next);
 
+void fade_text_in_out() {
     font_render_string(next, &font_info, "http://ktt----------ol.de", 15);
     fade_init(&fade, prev, next);
     frame_delay_init();
@@ -176,39 +234,6 @@ void foo() {
     }
 
     delay(2000);
-
-    disp_erase_value(prev, 0);
-    fade_init(&fade, next, prev);
-    frame_delay_init();
-    while (fade_step(&fade)) {
-        ticker_shift_display_buffer(next);
-        frame_delay();
-    }
-
-    font_string_shift_init(&shift_info, next, &font_info, msg);
-    frame_delay_init();
-    while (font_string_shift_step(&shift_info)) {
-        ticker_shift_display_buffer(next);
-        frame_delay();
-    }
-    ticker_shift_display_buffer(next);
-    frame_delay();
-
-    font_string_shift_reset(&shift_info);
-
-    disp_randomize_factor(prev, 5);
-
-    fade_init(&fade, next, prev);
-    frame_delay_init();
-    while (fade_step(&fade)) {
-        ticker_shift_display_buffer(next);
-        frame_delay();
-    }
-
-    gol_task_init(&gol_task, prev, next, 50);
-    while(gol_task_step(&gol_task)) {
-        ticker_shift_display_buffer(gol_task.next);
-    }
 
     disp_erase_value(prev, 0);
     fade_init(&fade, next, prev);
